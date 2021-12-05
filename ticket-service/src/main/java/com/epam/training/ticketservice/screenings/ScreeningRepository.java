671f8848a72e9c0ef214378098b89cba4ee7a5bf
Admadma <admadmaa77@gmail.com>
2021-12-06 00:55:30 +0100

    Added function to add and list screenings, without overlap checking

---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/screenings/ScreeningRepository.java
@@ -0,0 +1,2 @@
+package com.epam.training.ticketservice.screenings;public interface ScreeningRepository {
+}
---
+++ ticket-service/src/main/java/com/epam/training/ticketservice/screenings/Screeningservice.java
@@ -0,0 +1,2 @@
+package com.epam.training.ticketservice.screenings;public class Screeningservice {
+}
